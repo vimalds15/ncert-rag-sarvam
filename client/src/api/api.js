c76c035bd11c59b0de2dc0c43a34b5bc7cb42685
vimalds15 <antovimalands@gmail.com>
2024-09-28 15:02:27 +0530

    feat: "client: changed api base url"

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://ncert-rag-sarvam-production.up.railway.app/",
   headers: {
     "Content-Type": "application/json",
   },
